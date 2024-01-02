5bfad5ee5a7514329c75865842264804b9d3a812
mthirumalai2905 <mthirumalai2905@gmail.com>
2024-01-02 15:08:32 +0530

    advice_updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = { advice: '', images: [], bgImageIndex: 0 };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const { advice } = response.data.slip;
+      this.setState({ advice });
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  };
+
+  fetchBackground = async () => {
+    try {
+      const response = await axios.get("https://api.unsplash.com/photos/?client_id=fM3C9dmbJp1Equ8GZCJ4epM80FXL6G2pCpsMWUA2k-0&per_page=20");
+      const data = response.data;
+  
+      this.setState((prevState) => ({ bgImageIndex: (prevState.bgImageIndex + 1) % data.length, images: data }));
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+  
+  render() {
+    const { advice, images, bgImageIndex } = this.state;
+    const backgroundImage = images.length > 0 ? `url(${images[bgImageIndex].urls.full})` : '';
+  
+    return (
+      <div className='app' style={{ backgroundImage }}>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={() => { this.fetchAdvice(); this.fetchBackground(); }}>
+            <span>GIVE ME ADVICE</span>
+          </button>
+          <div className="social-icons">
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/images.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const images = ({images}) => {
+    console.log(images);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default images
